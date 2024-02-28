958ecd53550042471cf746a5119a53c6b75929b8
Learning-Pythong <lamostlaaa@gmail.com>
2024-02-28 20:52:05 +1100

    Importing project to GitHub

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/text-container.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color, {super.key});
+  final List<Color> color;
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: color, begin: startAlignment, end: endAlignment),
+      ),
+      child: const Center(child: TextContainer("NETFLIX")),
+    );
+  }
+}
---
+++ first_app/lib/text-container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextContainer extends StatelessWidget {
+  const TextContainer(this.text, {super.key});
+  final String text;
+  @override
+  Widget build(context) {
+    return Text(text,
+        style: const TextStyle(
+            color: Colors.red,
+            fontSize: 80,
+            fontWeight: FontWeight.w800,
+            fontFamily: "Bebas Neue"));
+  }
+}
