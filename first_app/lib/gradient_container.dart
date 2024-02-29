6a3e5e531839114824b21f481636198f1fd738e6
Learning-Pythong <lamostlaaa@gmail.com>
2024-02-29 15:27:36 +1100

    Stateful Widgets Intro

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/text-container.dart';
+import 'package:flutter/widgets.dart';
+import 'package:first_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -7,16 +9,16 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color, {super.key});
   final List<Color> color;
+
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: color, begin: startAlignment, end: endAlignment),
-      ),
-      child: Center(
-        child: Image.asset('assets/images/dice-2.png', width: 200),
-      ),
-    );
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: color, begin: startAlignment, end: endAlignment),
+        ),
+        child: const Center(
+          child: DiceRoller(),
+        ));
   }
 }
